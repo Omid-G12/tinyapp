3ce3a8916ed1432945403907b735158219c8a4d8
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-07 19:53:50 +0000

    POST route for edit function added

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const generateRandomString = function() {
   return Math.random().toString(36).substring(2, 8);
@@ -8,6 +10,8 @@ const generateRandomString = function() {
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -36,6 +40,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
+  //console.log(req.body);
   const templateVars = { id: req.params.id, longURL: urlDatabase[(req.params.id)] };
   res.render("urls_show", templateVars);
 });
@@ -56,6 +61,13 @@ app.post("/urls", (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
+});
+
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL;
+  //console.log(req.body.newURL);
+  res.redirect("/urls");
+
 });
 
 app.listen(PORT, () => {
